f43a15d4499f2c51b52e0b95c51509c9dc63d2cc
lotos580 <vothcicev_k@mail.ru>
2020-01-21 19:29:38 +0300

    Add files via upload

--- Task5_1.hs
+++ Task5_1.hs
@@ -9,7 +9,7 @@ data DList a = DNil
                 left :: (DList a), 
                 current :: a, 
                 right :: (DList a) 
-             }
+             } --deriving Show
 
 instance (Show a) => Show (DList a) where
     show it = "[" ++ showBody it ++ "]"
@@ -34,10 +34,32 @@ list2dlist' left (h: t) =
 
 -- Реализуйте функции индексирования, вставки и удаления элементов
 index :: DList a -> Int -> a
-index = todo
+index DNil i = error "index mismatch."
+index dl i = if i == 0 then current dl else 
+                                        if i > 0 then index (right dl) (i - 1)
+                                                 else index ( left dl) (i + 1)
+    
 
-insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt :: DList a -> Int -> a -> DList a 
+insertAt DNil i v 
+            | i == 0 = DCons{left=DNil, current=v, right=DNil}
+            | otherwise = error "index mismatch."
+insertAt (DCons l v r) i val  
+            | otherwise  = (DCons{left=l, current=v, right=( insertAt (r) (i - 1) val )})
+
+
+            
+keepTheList :: [DList a] -> DList a -> [DList a] 
+keepTheList xs DNil = xs
+keepTheList xs (DCons l v r) = xs ++ [(DCons l v r)] ++ (keepTheList [] (r))
+ 
+ 
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt DNil _ = DNil
+removeAt (DCons l _ DNil)  0 = DCons{left=(left l), current=(current l), right=DNil}   --  конечный элемент
+removeAt (DCons DNil _ r)  0 = DCons{left=DNil, current=(current r), right=(right r)}  --  начальный элемент 
+removeAt (DCons l v r) i = if i == 0 then DCons{left=l, current=(current r), right=(right r)}  
+                                     else DCons{left=l, current=v, right=(removeAt r (i - 1))}   --  поиск элемента и замена
+
+ 
